dd9114e2ffb40dd68745381d5a6f7144ca6add70
Ryan Quinn <rquinn@costar.com>
2023-12-13 11:00:56 -0500

    Works on test case

--- 2023/day13/main.py
+++ 2023/day13/main.py
@@ -40,10 +40,53 @@ def check_pattern_symmetry(pattern):
 
 columns_left = 0
 rows_above = 0
-for pattern in patterns:
+old_results = {}
+for i, pattern in enumerate(patterns):
     pat_columns_left, pat_rows_above = check_pattern_symmetry(pattern)
+    old_results[i] = (pat_columns_left, pat_rows_above)
     if pat_columns_left:
         columns_left += pat_columns_left
     if pat_rows_above:
         rows_above += pat_rows_above
+print(rows_above * 100 + columns_left)
+
+columns_left = 0
+rows_above = 0
+for i, pattern in enumerate(patterns):
+    max_cols_left = None
+    max_rows_above = None
+    for x, row in enumerate(pattern):
+        for y, _ in enumerate(row):
+            new_pattern = np.copy(pattern)
+            new_char = ''
+            if new_pattern[x, y] == '.':
+                new_char = '#'
+            else:
+                new_char = '.'
+            new_pattern[x, y] = new_char
+            pat_columns_left, pat_rows_above = check_pattern_symmetry(new_pattern)
+            old_columns_left, old_rows_above = old_results[i]
+            if not pat_columns_left and not pat_rows_above:
+                continue
+            else:
+                if pat_columns_left:
+                    if pat_columns_left == old_columns_left:
+                        pass
+                    elif not max_cols_left:
+                        max_cols_left = pat_columns_left
+                    else:
+                        max_cols_left = max(pat_columns_left, max_cols_left)
+                if pat_rows_above:
+                    if pat_rows_above == old_rows_above:
+                        pass
+                    elif not max_rows_above:
+                        max_rows_above = pat_rows_above
+                    else:
+                        max_rows_above = max(pat_rows_above, max_rows_above)
+    old_columns_left, old_rows_above = old_results[i]
+    if max_cols_left and max_cols_left != old_columns_left:
+        columns_left += max_cols_left 
+    if max_rows_above and max_rows_above != old_rows_above:
+        rows_above += max_rows_above
+    print(max_rows_above, max_cols_left)
 print(rows_above * 100 + columns_left)

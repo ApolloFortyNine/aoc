452776b96ce0d0831288b016262ef21fe80eaa78
Ryan Quinn <rquinn@costar.com>
2023-12-13 11:30:55 -0500

    Day 13 part 2

--- 2023/day13/main.py
+++ 2023/day13/main.py
@@ -9,7 +9,7 @@ for x in instructions_raw:
     new_pattern = np.array(split_by_line)
     patterns.append(new_pattern)
 
-def check_pattern_symmetry(pattern):
+def check_pattern_symmetry(pattern, old_pats=(-1,-1)):
     rows, cols = pattern.shape
     middle = cols // 2
     vertical = False
@@ -23,7 +23,10 @@ def check_pattern_symmetry(pattern):
         right_side_truncated = right_side[:, :min_cols]
         if np.array_equal(left_side_truncated, right_side_truncated):
             vertical = True
-            max_col = i
+            if i == old_pats[0]:
+                pass
+            else:
+                max_col = i
     horizontal = False
     max_row = None
     for i in range(0, rows):
@@ -35,7 +38,10 @@ def check_pattern_symmetry(pattern):
         bottom_side_truncated = bottom_side[:min_rows:, :]
         if np.array_equal(top_side_truncated, bottom_side_truncated):
             horizontal = True
-            max_row = i
+            if i == old_pats[1]:
+                pass
+            else:
+                max_row = i
     return (max_col, max_row)
 
 columns_left = 0
@@ -53,10 +59,14 @@ print(rows_above * 100 + columns_left)
 columns_left = 0
 rows_above = 0
 for i, pattern in enumerate(patterns):
+    if i == 98:
+        pass
     max_cols_left = None
     max_rows_above = None
     for x, row in enumerate(pattern):
         for y, _ in enumerate(row):
+            if x == 8:
+                pass
             new_pattern = np.copy(pattern)
             new_char = ''
             if new_pattern[x, y] == '.':
@@ -64,29 +74,25 @@ for i, pattern in enumerate(patterns):
             else:
                 new_char = '.'
             new_pattern[x, y] = new_char
-            pat_columns_left, pat_rows_above = check_pattern_symmetry(new_pattern)
             old_columns_left, old_rows_above = old_results[i]
+            pat_columns_left, pat_rows_above = check_pattern_symmetry(new_pattern, (old_columns_left, old_rows_above))
             if not pat_columns_left and not pat_rows_above:
                 continue
             else:
                 if pat_columns_left:
                     if pat_columns_left == old_columns_left:
                         pass
-                    elif not max_cols_left:
-                        max_cols_left = pat_columns_left
                     else:
-                        max_cols_left = max(pat_columns_left, max_cols_left)
+                        max_cols_left = pat_columns_left
                 if pat_rows_above:
                     if pat_rows_above == old_rows_above:
                         pass
-                    elif not max_rows_above:
-                        max_rows_above = pat_rows_above
                     else:
-                        max_rows_above = max(pat_rows_above, max_rows_above)
+                        max_rows_above = pat_rows_above
     old_columns_left, old_rows_above = old_results[i]
     if max_cols_left and max_cols_left != old_columns_left:
         columns_left += max_cols_left 
     if max_rows_above and max_rows_above != old_rows_above:
         rows_above += max_rows_above
-    print(max_rows_above, max_cols_left)
+    print(i, max_rows_above, max_cols_left)
 print(rows_above * 100 + columns_left)
